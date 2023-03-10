6e11b1257cd921c5e0467c3ac6bb7c301eac37ec
Mikkobelly <mikiakuta0529@gmail.com>
2023-03-10 02:47:15 +0000

    Created user-login-form component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -13,9 +14,13 @@ export class AppComponent {
 
   constructor(public dialog: MatDialog) { }
 
-  // Fuction to open the dialog when the signup button is clicked
+  // Open the signup dialog
   openUserRegistrationDialog(): void {
-    console.log('opened!')
-    this.dialog.open(UserRegistrationFormComponent, { width: '280px' });
+    this.dialog.open(UserRegistrationFormComponent, { width: '300px' });
+  }
+
+  // Open the login dialog
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, { width: '300px' });
   }
 }
